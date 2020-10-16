f01249c07a48cc243c7ed3ef40a415e6eedeefdb
shopvia <shopviaofficial@gmail.com>
2020-10-16 18:17:49 +0545

    Added Navbar and Footer files

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../../src/assets/footer.css";
+class Footer extends Component {
+  render() {
+    return (
+      <footer class="footer">
+        <div class="container">
+          <span class="text-muted">
+            &copy; Astranix Technologies Pvt. Ltd. 2020-2021
+          </span>
+        </div>
+      </footer>
+    );
+  }
+}
+export default Footer;
